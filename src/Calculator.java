12e447667f5feb63f26716c5fe97901826b9808a
Eugen <19strongman93@gmail.com>
2019-09-29 23:39:05 +0300

    multiply division sqrt was added

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,4 +3,19 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(int a) {return Math.sqrt(a);
+    }
 }
